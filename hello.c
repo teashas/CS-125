067045ffdabf103470fecf82cdcbce0a5f0e216c
teashas <74995287+teashas@users.noreply.github.com>
2022-05-25 11:15:24 -0700

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Greetings earthlings!\n");
+    printf("I was born in Bangladesh, and raised in Qatar. \n");
+    printf("I learned how to code in VHDL and MATLAB last semester.\n");
+    return 0; 
+
+}
+
