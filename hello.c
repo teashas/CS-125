31772868a7f00958d82e28ecdfd41688c3d44296
teashas <74995287+teashas@users.noreply.github.com>
2022-05-25 11:24:45 -0700

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,22 @@
+/*******************************************************************************
+*  Saima Teasha
+*  CS125
+*  program HW1
+*
+*  Write and submit a C program to display "Greetings earthlings" message.
+*  Print something about where you came from (where you grew up or where 
+*  you call home). Print something about your prior experience with 
+*  programming, and be specific about the programming language or tool that 
+*  you used.
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+    printf("Greetings earthlings!\n");
+    printf("I was born in Bangladesh, and raised in Qatar. \n");
+    printf("I learned how to code in VHDL and MATLAB last semester.\n");
+    return 0; 
+
+}
+
